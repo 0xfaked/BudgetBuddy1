b06be735b6d26ea16c4d6752b8d643e2f3371a7c
VAIBHAV GULVE <vaibhavgulve@VAIBHAVs-MacBook-Air.local>
2025-11-09 04:19:30 -0800

    build: transpile @arcjet/next to fix potential middleware ESM issues

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  transpilePackages: ["@react-email/components", "inngest"],
+  transpilePackages: ["@react-email/components", "inngest", "@arcjet/next"],
   images: {
     unoptimized: true,
   },
